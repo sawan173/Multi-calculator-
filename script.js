bff9f3c84ed843afe88750013cd5976778d4872a
sawan173 <sawanfarkale0@gmail.com>
2025-08-23 21:13:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.2 });
 
 cards.forEach(card => observer.observe(card));
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+      alert("Button clicked: " + btn.innerText);
+    });
+  });
+});
